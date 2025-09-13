6329f933fe07a6f3d1a6c437c4a0b8588104a716
harshilpatel05 <patelharshil2005@gmail.com>
2025-09-13 09:16:37 +0530

    removing previous fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
   useEffect(() => {
     const supabase = createClient()
 
-    //fetch all pdfs from bucket
     const fetchPdfs = async () => {
       const { data, error } = await supabase.storage.from("pdf").list("pdf")
       if (error) return
@@ -59,10 +58,10 @@ export default function Home() {
                 <li key={pdf.url}>
                   <button
                     className={`block w-full text-left px-3 py-2 rounded hover:bg-gray-200 ${selectedPdf === pdf.url
-                      ? "bg-blue-100 font-bold text-blue-700"
-                      : "text-gray-700"
+                        ? "bg-blue-100 font-bold text-blue-700"
+                        : "text-gray-700"
                       }`}
-                    onClick={() => setSelectedPdf(pdf.key)}
+                    onClick={() => setSelectedPdf(pdf.url)}
                   >
                     {pdf.key.slice(4, pdf.key.length - 4)}
                   </button>
@@ -70,6 +69,8 @@ export default function Home() {
               ))}
             </ul>
           </div>
+
+          {/* PDF viewer */}
           <div className="flex-1 md:flex-[2] border-b md:border-b-0 md:border-r min-h-[400px]">
             {selectedPdf ? (
               // <iframe
@@ -78,21 +79,13 @@ export default function Home() {
               //   height="100%"
               //   className="border-0 min-h-[400px] md:h-full"
               // />
-              // <iframe
-              //   src={`https://docs.google.com/viewer?url=${encodeURIComponent(selectedPdf)}&embedded=true`}
-              //   width="100%"
-              //   height="800px"
-              //   className="border-0"
-              // />
               <iframe
-                src={`https://docs.google.com/viewer?url=${encodeURIComponent(
-                  `${window.location.origin}/api/pdf/${selectedPdf}`
-                )}&embedded=true`}
+                src={`https://docs.google.com/viewer?url=${encodeURIComponent(selectedPdf)}&embedded=true`}
                 width="100%"
                 height="800px"
                 className="border-0"
               />
-
+              
 
             ) : (
               <div className="flex items-center justify-center h-full text-gray-500">
