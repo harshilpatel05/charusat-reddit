3b00dd4867aee509f10fa44e0ddf63937f58281f
harshilpatel05 <patelharshil2005@gmail.com>
2025-09-14 14:33:39 +0530

    build fix

--- src/app/api/push-subscribe/route.ts
+++ src/app/api/push-subscribe/route.ts
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 import { cookies } from 'next/headers';
 
 export async function POST(req: NextRequest) {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const token = cookieStore.get("authToken")?.value;
   if (!token) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   const secret = process.env.JWT_SECRET;
