05fc3d2cedf1077a72e37456171e12dcfb8af47f
harshilpatel05 <patelharshil2005@gmail.com>
2025-09-14 15:06:25 +0530

    build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import dynamic from "next/dynamic";
 import ClientLayout from "@/components/ClientLayout";
 
 const geistSans = Geist({
